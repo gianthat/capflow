2345edc6afab71dff78585315938c630eef774ff
Spencer Markowski <spencer@theablefew.com>
2011-08-18 20:30:59 -0400

    Adds helpers directly into namespace until I can find a solid way to test capistrano

--- lib/capistrano/tampon.rb
+++ lib/capistrano/tampon.rb
@@ -4,7 +4,8 @@ require 'stringex'
 
 module Capistrano
   class Tampon
-    include Capistrano::Helpers::TamponHelper
+    # I gave up for now, since namespace can't call include
+ #   include Capistrano::Helpers::TamponHelper
 
     def self.load_into(capistrano_configuration)
       capistrano_configuration.load do
@@ -12,7 +13,63 @@ module Capistrano
         before "tampon:calculate_tag", "tampon:verify_up_to_date"
 
         namespace :tampon do
-          
+              
+        def who
+          (`git config user.name` || `whoami`).chomp
+        end
+
+        def tags
+          `git tag`.split("\n").compact
+        end
+
+        def non_release_tags
+          tags - releases 
+        end
+
+        def current_branch
+          branches.select{|b| b =~ /^\*\s/}.first.gsub(/^\*\s/,"")
+        end
+
+        def branches
+         `git branch --no-color`.split("\n")
+        end
+
+        def version_tag_prefix
+          `git config gitflow.prefix.versiontag`.split("\n").first
+        end
+
+        def releases
+          tags.select{|t| t =~ /^#{version_tag_prefix}(\d+)/}
+        end
+
+        def latest_release
+          releases.sort{|x,y| x.split(version_tag_prefix).last <=> y.split(version_tag_prefix).last}.last
+        end
+
+        def available_tags
+          Capistrano::CLI.ui.say "Available Tags:"
+          Capistrano::CLI.ui.say "#{non_release_tags.join("\n")}"
+        end
+
+        def available_releases
+          Capistrano::CLI.ui.say "\nAvailable Releases:"
+          Capistrano::CLI.ui.say "#{releases.join("\n")}"
+        end
+
+        def banner
+
+          <<-BANNER
+\nTampon for Gitflow
+  ,-------------.
+ (o) _ __ _____  )--.
+  `-------------'    )
+                    (     /
+                     `---'
+          BANNER
+        end
+
+
+
           def deploy_from
             if stage == :production
               from_destination = Capistrano::CLI.ui.ask "\nRelease to deploy: [#{current_branch}] ".color(:yellow).bright.to_url
